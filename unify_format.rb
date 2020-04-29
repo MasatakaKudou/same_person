5650f548b1060c7caff8370aebfb12813bf53247
MasatakaKudou <b1017027@fun.ac.jp>
2020-04-29 14:32:45 +0900

    [add]配列を用いて、ハイフンと空白とそれ以外のパターンを確認できるようにした

--- unify_format.rb
+++ unify_format.rb
@@ -1,14 +1,14 @@
 class UnifyFormat
-  def self.unify_format(phone_number)
-    case phone_number
-    when /0[7-9]0(-| |)[1-9]\d{3}(-| |)\d{4}/
-      puts phone_number.gsub(/-| |/, '')
-    else
-      puts "何にも一致しないです"
-    end
+  def self.unify_format(phone_numbers)
+    phone_numbers.each {|phone_number|
+      case phone_number
+      when /0[7-9]0(-| |)[1-9]\d{3}(-| |)\d{4}/
+        puts phone_number.gsub(/-| |/, '')
+      else
+        puts "何にも一致しないです"
+      end
+    }
   end
 end
 
-UnifyFormat.unify_format('090-1931-6065')
-UnifyFormat.unify_format('090 1931 6065')
-UnifyFormat.unify_format('090~1931~6065')
+UnifyFormat.unify_format(['090-1931-6065', '090 1931 6065', '090~1931~6065'])

c9f46a83a5793084864f41a3ed99f614bd845a0f
MasatakaKudou <b1017027@fun.ac.jp>
2020-04-29 14:00:06 +0900

    [add]ハイフンと空白に対応し、それ以外は対応しないように

--- unify_format.rb
+++ unify_format.rb
@@ -1,9 +1,7 @@
 class UnifyFormat
   def self.unify_format(phone_number)
-    puts "フォーマットした結果は、以下の通りです。"
     case phone_number
     when /0[7-9]0(-| |)[1-9]\d{3}(-| |)\d{4}/
-      puts "携帯電話です"
       puts phone_number.gsub(/-| |/, '')
     else
       puts "何にも一致しないです"
@@ -12,3 +10,5 @@ class UnifyFormat
 end
 
 UnifyFormat.unify_format('090-1931-6065')
+UnifyFormat.unify_format('090 1931 6065')
+UnifyFormat.unify_format('090~1931~6065')

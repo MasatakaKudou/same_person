a32922cef975c31f18ffb25437af261e7e2eb9eb
MasatakaKudou <b1017027@fun.ac.jp>
2020-04-29 21:26:00 +0900

    [add]引数をstringに変更した

--- unify_format.rb
+++ unify_format.rb
@@ -1,5 +1,5 @@
 class PhoneNumberConvert
-  def convert(phone_numbers)
+  def convert(phone_number)
     patterns = [
       /0\d(-| |)[2-9]\d{3}(-| |)\d{4}/, #固定電話 [0]
       /0\d{2}(-| |)[2-9]\d{2}(-| |)\d{4}/, #固定電話 [1]
@@ -7,26 +7,27 @@ class PhoneNumberConvert
       /0\d{4}(-| |)[2-9](-| |)\d{4}/, #固定電話 [3]
       /0[5789]0(-| |)[1-9]\d{3}(-| |)\d{4}/, #携帯電話 or IP電話 [4]
     ]
-    phone_numbers.map {|phone_number|
-      case phone_number
-      when patterns[0], patterns[1], patterns[2], patterns[3], patterns[4]
-        phone_number.gsub(/-| |/, '')
-      else
-        nil
-      end
-    }
+    case phone_number
+    when patterns[0], patterns[1], patterns[2], patterns[3], patterns[4]
+      phone_number.gsub(/-| |/, '')
+    else
+      nil
+    end
   end
 end
 
 phone_number = PhoneNumberConvert.new
-p phone_number.convert(
-  [
-    '090-1111-1111', '090 1111 1111', #携帯電話
-    '050-1111-1111', '050 1111 1111', #IP電話
-    '01-2111-1111', '01 2111 1111', #固定電話
-    '011-211-1111', '011 211 1111', #固定電話
-    '0111-21-1111', '0111 21 1111', #固定電話
-    '01111-2-1111', '01111 2 1111', #固定電話
-    '11-1111-1111', '11 1111 1111' #nilパターン
-  ]
-)
+p phone_number.convert('090-1111-1111') #携帯電話
+p phone_number.convert('090 1111 1111') #携帯電話
+p phone_number.convert('050-1111-1111') #IP電話
+p phone_number.convert('050 1111 1111') #IP電話
+p phone_number.convert('01-2111-1111') #固定電話
+p phone_number.convert('01 2111 1111') #固定電話
+p phone_number.convert('011-211-1111') #固定電話
+p phone_number.convert('011 211 1111') #固定電話
+p phone_number.convert('0111-21-1111') #固定電話
+p phone_number.convert('0111 21 1111') #固定電話
+p phone_number.convert('01111-2-1111') #固定電話
+p phone_number.convert('01111 2 1111') #固定電話
+p phone_number.convert('11-1111-1111') #固定電話
+p phone_number.convert('11 1111 1111') #nilパターン

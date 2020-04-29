146ffb97496444aec759c05cc21f11064f34a441
MasatakaKudou <b1017027@fun.ac.jp>
2020-04-29 17:26:09 +0900

    [add]mapメソッドを使用して、リファクタリング

--- unify_format.rb
+++ unify_format.rb
@@ -1,5 +1,5 @@
-class UnifyFormat
-  def self.unify_format(phone_numbers)
+class PhoneNumberConvert
+  def convert(phone_numbers)
     patterns = [
       /0\d(-| |)[2-9]\d{3}(-| |)\d{4}/, #固定電話 [0]
       /0\d{2}(-| |)[2-9]\d{2}(-| |)\d{4}/, #固定電話 [1]
@@ -7,18 +7,19 @@ class UnifyFormat
       /0\d{4}(-| |)[2-9](-| |)\d{4}/, #固定電話 [3]
       /0[5789]0(-| |)[1-9]\d{3}(-| |)\d{4}/, #携帯電話 or IP電話 [4]
     ]
-    phone_numbers.each {|phone_number|
+    phone_numbers.map {|phone_number|
       case phone_number
       when patterns[0], patterns[1], patterns[2], patterns[3], patterns[4]
-        puts phone_number.gsub(/-| |/, '')
+        phone_number.gsub(/-| |/, '')
       else
-        puts "何にも一致しないです"
+        "何にも一致しないです"
       end
     }
   end
 end
 
-UnifyFormat.unify_format(
+phone_number = PhoneNumberConvert.new
+puts phone_number.convert(
   [
     '090-1111-1111', '090 1111 1111', #携帯電話 [0]
     '050-1111-1111', '050 1111 1111', #IP電話 [1]
